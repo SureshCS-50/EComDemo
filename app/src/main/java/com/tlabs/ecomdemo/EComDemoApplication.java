feb98c87b5f8112e899df080c403d78bd28e8839
Sureshkumar <sureshkumar@tenmiles.com>
2016-11-21 13:28:12 +0530

    added Fabric Answers

--- app/src/main/java/com/tlabs/ecomdemo/EComDemoApplication.java
+++ app/src/main/java/com/tlabs/ecomdemo/EComDemoApplication.java
@@ -1,10 +1,23 @@
 package com.tlabs.ecomdemo;
 
+import com.crashlytics.android.Crashlytics;
 import com.orm.SugarApp;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Sureshkumar on 07/11/16.
  */
 
 public class EComDemoApplication extends SugarApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
+    }
 }
